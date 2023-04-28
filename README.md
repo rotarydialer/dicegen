0385fa31ed96a04cdda9414a9a990667a52b99af
Chris <github@rotarydial.com>
2023-04-28 00:51:27 -0400

    Implement same logic in NodeJS (#2)
    * chore: npm initialize
    
    * feat: dicegen args; file read
    
    * config: VS Code linter settings
    
    * config: .gitignore for node
    
    * deps: yargs
    
    * feat: working single phrase generation
    
    * docs: Usage to README
    
    * feat: generate multiple phrases
    
    * docs: usage with optional flags
    
    * version: v0.2.0
    
    * refactor: idiomatic function

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # dicegen
 
 A simple passphrase generator, based on [Diceware](https://en.wikipedia.org/wiki/Diceware).
+
+## Usage
+
+Generate a single passphrase, with the default length of 6 words:
+
+```
+$ node dicegen
+
+Generating a passphrase with 6 words...
+
+     lease bundy bird oc freon music
+```
+
+Change the length of the passphrase with the `--length` | `-l` flag:
+
+```
+$ node dicegen --length 3
+
+Generating a passphrase with 3 words...
+
+     germ sung muffin
+```
+
+### Other options
+
+Generate multiple passphrases with the `--phrases` | `-p` flag:
+
+```
+$ node dicegen --phrases 3
+Generating 3 passphrases, each with 6 words...
+
+     qualm xm saxon gadget dig peel rapid
+     crump chime seal erato 85th glued thyme
+     chile their rail brady eat wring thine
+```
---
+++ dicegen.js
@@ -0,0 +1,119 @@
+const yargs = require("yargs");
+
+const generate = (wordCount, wordlist, phraseCount, outFile) => {
+  // TODO: implement output to file
+  if (phraseCount === 1) {
+    console.log(`Generating a passphrase with ${wordCount} words...`);
+  } else {
+    console.log(
+      `Generating ${phraseCount} passphrases, each with ${wordCount} words...`
+    );
+  }
+  console.log();
+
+  let codeWordMap = new Map();
+  codeWordMap = loadWordlist(wordlist);
+
+  const phrases = generateNPhrases(phraseCount, wordCount, codeWordMap);
+
+  phrases.forEach((phrase) => {
+    console.log(`     ${phrase}`);
+  });
+
+  console.log();
+  console.log("Done.");
+};
+
+const generateNPhrases = (phraseCount, wordCount, codeWordMap) => {
+  let phrases = [];
+
+  if (phraseCount === 1) return [generatePhrase(wordCount, codeWordMap)];
+
+  for (let c = 1; c <= phraseCount; c++) {
+    phrases.push(generatePhrase(wordCount, codeWordMap));
+  }
+
+  return phrases;
+};
+
+const generatePhrase = (wordCount, codeWordMap) => {
+  let codesRandomized = [];
+
+  for (let i = 0; i < wordCount; i++) {
+    codesRandomized.push(concatDieRolls());
+  }
+
+  let phrase = [];
+
+  for (let j = 0; j < codesRandomized.length; j++) {
+    phrase.push(codeWordMap.get(codesRandomized[j]));
+  }
+
+  return phrase.join(" ");
+};
+
+const loadWordlist = (filename) => {
+  const fs = require("fs");
+  const lines = fs
+    .readFileSync(filename)
+    .toString()
+    .split("\n")
+    .filter((line) => line.length > 0);
+
+  let fileMap = new Map();
+  lines.forEach((line) => {
+    const linePair = line.split("\t");
+    fileMap.set(linePair[0], linePair[1]);
+  });
+
+  return fileMap;
+};
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const concatDieRolls = (numSides = 6, numDigits = 5) => {
+  let rolls = [];
+  for (let i = 0; i < numDigits; i++) {
+    rolls.push(getRandomInt(1, numSides));
+  }
+
+  return rolls.join("");
+};
+
+const args = yargs
+  .command(
+    "*",
+    "Generate a passphrase, based on Diceware. (https://en.wikipedia.org/wiki/Diceware)",
+    (yargs) => {}
+  )
+  .option("length", {
+    alias: "l",
+    describe: "number of words to include in the phrase",
+    default: 6,
+  })
+  .option("phrases", {
+    alias: "p",
+    describe: "number of phrases to generate",
+    default: 1,
+  })
+  .option("corpus", {
+    alias: "c",
+    describe: "wordlist (input) filename",
+    default: "resources/wordlist.txt",
+  })
+  .option("output", {
+    alias: "o",
+    describe: "output filename",
+  })
+  .help().argv;
+
+const wordCount = args.length;
+const phraseCount = args.phrases;
+const wordlist = args.corpus;
+const outFile = args.output;
+
+generate(wordCount, wordlist, phraseCount, outFile);
